f67ea62d73f93fe8d66fad1249c1773fa4fdfe98
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2017-03-21 16:44:50 +0800

    dropped default config test

--- _pytest/test_config.py
+++ _pytest/test_config.py
@@ -2,45 +2,6 @@ import tempfile
 
 from rasa_nlu.config import RasaNLUConfig
 import json
-import os
-import logging
-
-defaults = {
-  "config": "config.json",
-  "data": None,
-  "emulate": None,
-  "language": "en",
-  "log_file": None,
-  "log_level": logging.INFO,
-  "mitie_file": os.path.join("data", "total_word_feature_extractor.dat"),
-  "num_threads": 1,
-  "fine_tune_spacy_ner": False,
-  "max_number_of_ngrams": 7,
-  "path": os.path.join(os.getcwd(), "models"),
-  "port": 5000,
-  "server_model_dirs": None,
-  "token": None,
-  "pipeline": [],
-  "response_log": os.path.join(os.getcwd(), "logs"),
-  "luis_data_tokenizer": None,
-}
-
-
-def test_default_config():
-    final_config = RasaNLUConfig()
-    assert dict(final_config.items()) == defaults
-
-
-def test_blank_config():
-    file_config = {}
-    cmdline_args = {}
-    env_vars = {}
-    with tempfile.NamedTemporaryFile(suffix="_tmp_config_file.json") as f:
-        f.write(json.dumps(file_config))
-        f.flush()
-        final_config = RasaNLUConfig(f.name, env_vars, cmdline_args)
-        assert dict(final_config.items()) == defaults
-
 
 def test_file_config_unchanged():
     file_config = {"path": "/path/to/dir"}
